99a58a3bd0b9465d51dfd3ac07a665d3a17162c0
Steven John Bolton <steviebolton@gmail.com>
2018-07-10 07:21:42 +0000

    first dabble in python

---
+++ trypython.py
@@ -0,0 +1,144 @@
+
+# to activate python just type: "python3"
+# to see which version of python being used:* Python —version
+
+
+# # prints 10
+# x = 10
+# print(x)
+
+
+
+# x = 10
+# print(x)
+
+# a = 2
+# b = 6
+# y = a+b
+# print(y)
+
+# print(10+10)
+
+# print(3*4)
+# print(35/4)
+# print(35-4)
+
+
+# x=2
+# print(x**x)
+
+
+# a = type(0)
+# print(a) 
+
+# will return <class 'int'>
+
+# s = type("hello")
+# print(s)
+
+# s = "hello "
+# c = "what have we here?"
+# b = s*5
+# print(b + c)
+# will return:
+#  <class 'str'>
+
+# s = "hello"
+# str = ""
+# for i in s:
+#     str = i + str
+# print(str)
+
+# # 22. Evaluate the expression s + t
+# s= "hello"
+# t= " world"
+# a= s+t
+# print(a)
+
+# 24. Create a variable a with a value of True (Find out what integer value is behind True. Demonstrate it)
+
+# a = True
+# print(int(a))
+
+# 25. Create a variable b with a value of False (Find out what integer value is behind True. Demonstrate it)
+
+# 27. Evaluate the expression 
+
+# a = True
+# b = False
+
+# a=1
+# b=0
+
+# a = True
+# b = False 
+
+# print(a or b)    
+# print(a or a)    
+# print(a or b)    
+# print(b or a)    
+# print(b or b)  
+
+#     print(T and T)    
+#     print(T and F)    
+#     print(F and T)    
+#     print(F and F)
+# 41. Write a python program that asks the user to enter their name, allows them to enter text,
+# and then says hello as follows:
+
+
+# name = input('Enter your name: ')
+# print("Hello", name)
+
+# quest1= (input('Enter First Number:3 '))
+# return quest1
+# quest2= (input('Enter Second Number:5 '))
+# return quest2
+# answer = quest1 + quest2
+# print(quest1 " plus" quest1 "= " answer)
+
+
+# num1 = input('Enter first number:3 ')
+# num2 = input('Enter second number:5 ')
+
+# # Add two numbers
+# sum = float(num1) + float(num2)
+
+
+
+# 42. Write a program that asks the user to enter two numbers, the prints the result of adding them.
+
+# Enter First Number: 3
+# Enter Second Number: 5
+# 3 plus 5 = 8
+
+# print('{0} plus {1} = {2}'.format(num1, num2, sum))
+
+# 43. Write program that allows the user to enter some text.
+# Display that text in all UPPER CASE
+
+
+# capitalized_string = input('Enter some text ')
+# capitalized_string = capitalize(capitalized_string)
+
+
+# print( "capitalize")
+
+# a = 5
+# if a > 10:
+#     print 'a is greater than 10'
+#     if a >= 15:
+#         print 'a is also at least 15'
+# elif a > 5:
+#     print 'a is greater than 5 but not greater than 10'
+# else:
+#     print 'no condition matched'
+#     print 'so a is 5 or less'
+
+
+def happyBirthdayEmily(): #program does nothing as written
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear Emily.")
+    print("Happy Birthday to you!")
+
